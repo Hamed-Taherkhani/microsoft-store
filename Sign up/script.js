69743edff5e4b7de83a5395e79a7fe5b5f85e35f
Hamed Taherkhani <hamedtahi7@gmail.com>
2021-09-22 19:05:40 +0330

    Add input image file to html file; and handle input with js, some style to get better performance

--- Sign up/script.js
+++ Sign up/script.js
@@ -1,17 +1,51 @@
-const signUpBtn = document.querySelector("button");
-const formElement = document.querySelector("form");
-const url = "??????????????";
+const form = document.querySelector("form"),
+  fileInput = document.querySelector("#avatar");
 
-signUpBtn.addEventListener("submit", eventHandler);
+fileInput.addEventListener("change", eventHandler);
 function eventHandler() {
-  this.preventDefault();
-  const http = new XMLHttpRequest();
-  const formData = new FormData(formElement);
+  const file = this.files[0],
+    reader = new FileReader();
 
-  http.onerror = () => {
-    alert("Something wrong happen !!!\nPlease try late.");
-  };
+  reader.readAsDataURL(file);
+  reader.addEventListener("load", resizeImage);
+  function resizeImage() {
+    const result = this.result;
 
-  http.open("Post", url);
-  http.send(formData);
+    const imgElement = document.createElement("img");
+    imgElement.src = result;
+    imgElement.onload = function (e) {
+      const canvas = document.createElement("canvas");
+      const MAX_WIDTH = 200;
+
+      const scaleSize = MAX_WIDTH / e.target.width;
+      canvas.width = MAX_WIDTH;
+      canvas.height = e.target.height * scaleSize;
+
+      const ctx = canvas.getContext("2d");
+      ctx.drawImage(e.target, 0, 0, canvas.width, canvas.height);
+      const srcEncoded = ctx.canvas.toDataURL(e.target, "image/jpeg");
+
+      // you can send srcEncoded to the server
+      const resizedImage = document.createElement("img");
+      resizedImage.src = srcEncoded;
+      form.append(resizedImage);
+
+      let imageFile = dataURLtoFile(srcEncoded, "avatar.png");
+      fileInput.file = imageFile;
+    };
+  }
+
+  function dataURLtoFile(dataurl, filename) {
+    var arr = dataurl.split(","),
+      mime = arr[0].match(/:(.*?);/)[1],
+      bstr = atob(arr[1]),
+      n = bstr.length,
+      u8arr = new Uint8Array(n);
+
+    while (n--) {
+      u8arr[n] = bstr.charCodeAt(n);
+    }
+
+    return new File([u8arr], filename, { type: mime });
+  }
 }
